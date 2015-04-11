efabd4e85442fb43d237b49c2c9ad9657da36c0e
Dylan O'Brennan <the.dob@gmail.com>
2015-04-11 11:23:38 -0500

    registered params whitelist for all app/admin/rb's

--- app/admin/customer.rb
+++ app/admin/customer.rb
@@ -1,6 +1,7 @@
 ActiveAdmin.register Customer do
 
-
+  permit_params :name, :address, :city, :country, :postal_code, :email, :province_id, :created_at, :updated_at
+  
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
