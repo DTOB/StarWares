af1184c8cc8cf36e13249501740be3ff87baeafe
Dylan O'Brennan <the.dob@gmail.com>
2015-04-12 13:49:51 -0500

    created header and footer partials, reworked products/product pages.  started on basic SCSS

--- app/admin/product.rb
+++ app/admin/product.rb
@@ -1,6 +1,7 @@
 ActiveAdmin.register Product do
 
-
+  permit_params :model, :description, :price, :stock_quantity, :image
+  
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
